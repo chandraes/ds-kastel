9734c15776f436ff3364e80bbba71ba90f3a6d96
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-05-03 17:17:09 +0700

    Refactor BahanBakuController.php and index.blade.php for Bahan Baku module

--- resources/views/billing/form-transaksi/bahan-baku/keranjang.blade.php
+++ resources/views/billing/form-transaksi/bahan-baku/keranjang.blade.php
@@ -1,10 +1,15 @@
-<div class="modal fade" id="keranjangBelanja" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="keranjangTitle" aria-hidden="true">
+<div class="modal fade" id="keranjangBelanja" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"
+    role="dialog" aria-labelledby="keranjangTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="keranjangTitle">Keranjang</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            @php
+            $diskon = 0;
+            $ppn = 0;
+            @endphp
             <div class="modal-body">
                 <table class="table table-bordered">
                     <thead class="table-success">
@@ -28,12 +33,14 @@
                             <td class="text-center align-middle">{{$b->satuan->nama}}</td>
                             <td class="text-center align-middle">{{number_format($b->harga, 0, ',','.')}}</td>
                             <td class="text-center align-middle">{{number_format($b->add_fee, 0, ',','.')}}</td>
-                            <td class="text-center align-middle">{{number_format($b->total + $b->add_fee, 0, ',','.')}}</td>
+                            <td class="text-end align-middle">{{number_format($b->total + $b->add_fee, 0, ',','.')}}
+                            </td>
                             <td class="text-center align-middle">
                                 <form action="" method="post">
                                     @csrf
                                     @method('delete')
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                                    <button type="submit" class="btn btn-danger btn-sm"><i
+                                            class="fa fa-trash"></i></button>
                                 </form>
                             </td>
                         </tr>
@@ -43,71 +50,129 @@
                         <tr>
                             <td class="text-center align-middle"></td>
                             <td class="text-center align-middle"></td>
-                            <td class="text-center align-middle">{{count($keranjang) > 0 ? number_format($keranjang->sum('jumlah'), 0, ',','.') : ''}}</td>
+                            <td class="text-center align-middle">{{count($keranjang) > 0 ?
+                                number_format($keranjang->sum('jumlah'), 0, ',','.') : ''}}</td>
                             <td class="text-center align-middle"></td>
-                            <td class="text-center align-middle">{{count($keranjang) > 0 ? number_format($keranjang->sum('harga'), 0, ',','.') : ''}}</td>
-                            <td class="text-center align-middle">{{count($keranjang) > 0 ? number_format($keranjang->sum('add_fee'), 0, ',','.') : ''}}</td>
-                            <td class="text-center align-middle">{{count($keranjang) > 0 ? number_format($keranjang->sum('total'), 0, ',','.') : ''}}</td>
+                            <td class="text-center align-middle">{{count($keranjang) > 0 ?
+                                number_format($keranjang->sum('harga'), 0, ',','.') : ''}}</td>
+                            <td class="text-center align-middle">{{count($keranjang) > 0 ?
+                                number_format($keranjang->sum('add_fee'), 0, ',','.') : ''}}</td>
+                            <td class="text-end align-middle">{{count($keranjang) > 0 ?
+                                number_format($keranjang->sum('total'), 0, ',','.') : ''}}</td>
+                            <td class="text-center align-middle"></td>
+                        </tr>
+                        <tr>
+                            <td class="text-end align-middle" colspan="6">Total</td>
+                            <td class="text-end align-middle">{{count($keranjang) > 0 ?
+                                number_format($keranjang->sum('total') + $keranjang->sum('add_fee'), 0, ',','.') : ''}}
+                            </td>
+                            <td class="text-center align-middle"></td>
+                        </tr>
+                        <tr>
+                            <td class="text-end align-middle" colspan="6">PPN</td>
+                            <td class="text-end align-middle">{{count($keranjang) > 0 ?
+                                number_format($keranjang->sum('total') + $keranjang->sum('add_fee'), 0, ',','.') : ''}}
+                            </td>
+                            <td class="text-center align-middle"></td>
+                        </tr>
+                        <tr>
+                            <td class="text-end align-middle" colspan="6">Diskon</td>
+                            <td class="text-end align-middle" id="tdDiskon">
+                                0
+                            </td>
+                            <td class="text-center align-middle"></td>
+                        </tr>
+                        <tr>
+                            <td class="text-end align-middle" colspan="6">PPN</td>
+                            <td class="text-end align-middle">
+                                0
+                            </td>
+                            <td class="text-center align-middle"></td>
+                        </tr>
+                        <tr>
+                            <td class="text-end align-middle" colspan="6">Grand Total</td>
+                            <td class="text-end align-middle">
+                                0
+                            </td>
                             <td class="text-center align-middle"></td>
                         </tr>
                     </tfoot>
                 </table>
                 <form action="" method="get" id="beliBarang">
                     <div class="row">
-                        <div class="col-12">
+                        <div class="col-md-4">
                             <div class="mb-3">
-                              <label for="uraian" class="form-label">Uraian</label>
-                              <input type="text"
-                                class="form-control" name="uraian" id="uraian" aria-describedby="helpId" placeholder="" required>
+                                <label for="uraian" class="form-label">Uraian</label>
+                                <input type="text" class="form-control" name="uraian" id="uraian"
+                                    aria-describedby="helpId" placeholder="" required>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="mb-3">
+                                <label for="uraian" class="form-label">Apakah menggunakan PPn?</label>
+                                <select class="form-select" name="ppn" id="ppn">
+                                    <option value="">-- Pilih Salah Satu --</option>
+                                    <option value="1">Dengan PPn</option>
+                                    <option value="0">Tanpa PPn</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="mb-3">
+                                <label for="diskon" class="form-label">Diskon</label>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1">Rp</span>
+                                    <input type="text" class="form-control" name="diskon" id="diskon"
+                                        aria-describedby="helpId" placeholder="" required value="0">
+                                </div>
                             </div>
                         </div>
                     </div>
-                <hr>
-                <h2>
-                    Transfer Ke
-                </h2>
-                <br>
-                <div class="row">
-                    <div class="col-md-4 mb-3">
-                        <label for="transfer_ke" class="form-label">Nama Rekening</label>
-                        <input type="text" class="form-control @if ($errors->has('transfer_ke'))
+                    <hr>
+                    <h2>
+                        Transfer Ke
+                    </h2>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-4 mb-3">
+                            <label for="transfer_ke" class="form-label">Nama Rekening</label>
+                            <input type="text" class="form-control @if ($errors->has('transfer_ke'))
                         is-invalid
                     @endif" name="transfer_ke" id="transfer_ke" value="{{old('transfer_ke')}}" maxlength="15" required>
-                        @if ($errors->has('transfer_ke'))
-                        <div class="invalid-feedback">
-                            {{$errors->first('transfer_ke')}}
+                            @if ($errors->has('transfer_ke'))
+                            <div class="invalid-feedback">
+                                {{$errors->first('transfer_ke')}}
+                            </div>
+                            @endif
                         </div>
-                        @endif
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="bank" class="form-label">Bank</label>
-                        <input type="text" class="form-control @if ($errors->has('bank'))
+                        <div class="col-md-4 mb-3">
+                            <label for="bank" class="form-label">Bank</label>
+                            <input type="text" class="form-control @if ($errors->has('bank'))
                         is-invalid
                     @endif" name="bank" id="bank" value="{{old('bank')}}" maxlength="10" required>
-                        @if ($errors->has('bank'))
-                        <div class="invalid-feedback">
-                            {{$errors->first('bank')}}
+                            @if ($errors->has('bank'))
+                            <div class="invalid-feedback">
+                                {{$errors->first('bank')}}
+                            </div>
+                            @endif
                         </div>
-                        @endif
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="no_rekening" class="form-label">Nomor Rekening</label>
-                        <input type="text" class="form-control @if ($errors->has('no_rekening'))
+                        <div class="col-md-4 mb-3">
+                            <label for="no_rekening" class="form-label">Nomor Rekening</label>
+                            <input type="text" class="form-control @if ($errors->has('no_rekening'))
                         is-invalid
                     @endif" name="no_rekening" id="no_rekening" value="{{old('no_rekening')}}" required>
-                        @if ($errors->has('no_rekening'))
-                        <div class="invalid-feedback">
-                            {{$errors->first('no_rekening')}}
+                            @if ($errors->has('no_rekening'))
+                            <div class="invalid-feedback">
+                                {{$errors->first('no_rekening')}}
+                            </div>
+                            @endif
                         </div>
-                        @endif
                     </div>
-                </div>
             </div>
 
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-
-                    <button type="submit" class="btn btn-primary">Beli Barang</button>
+                <button type="submit" class="btn btn-primary">Beli Barang</button>
                 </form>
             </div>
         </div>
