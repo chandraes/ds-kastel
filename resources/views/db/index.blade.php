89b31494df853d9346430dc51ec654376fa16962
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-06-13 16:08:53 +0700

    Update index.blade.php to improve category layout and add new categories

--- resources/views/db/index.blade.php
+++ resources/views/db/index.blade.php
@@ -35,7 +35,7 @@
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
             <a href="{{route('db.pajak')}}" class="text-decoration-none">
                 <img src="{{asset('images/pajak.svg')}}" alt="" width="70">
-                <h5 class="mt-2">PAJAK</h5>
+                <h5 class="mt-2">PERSENTASE<br>PAJAK</h5>
             </a>
         </div>
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
