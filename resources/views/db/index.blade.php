fcd8b0b70f844b3868fe6a7f0d0791574ea553e5
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-06-14 18:35:39 +0700

    Update index.blade.php to improve category layout and add "DAFTAR & KONVERSI BAHAN BAKU"

--- resources/views/db/index.blade.php
+++ resources/views/db/index.blade.php
@@ -82,7 +82,7 @@
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
             <a href="{{route('db.bahan-baku')}}" class="text-decoration-none">
                 <img src="{{asset('images/bahan-baku.svg')}}" alt="" width="70">
-                <h5 class="mt-2">DAFTAR<br>BAHAN BAKU</h5>
+                <h5 class="mt-2">DAFTAR & KONVERSI<br>BAHAN BAKU</h5>
             </a>
         </div>
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
