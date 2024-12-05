0d9505c30b81fe90c73bcfb80cad57cf711e0125
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-12-05 19:05:38 +0700

    Fix route reference for invoice detail link in PPN Keluaran view

--- resources/views/pajak/ppn-keluaran/index.blade.php
+++ resources/views/pajak/ppn-keluaran/index.blade.php
@@ -96,7 +96,7 @@
                     <td class="text-center align-middle">{{$d->invoiceJual ? $d->invoiceJual->tanggal : '-'}}</td>
                     <td class="text-center align-middle">
                         @if ($d->invoiceJual)
-                        <a href="{{route('billing.invoice-konsumen.detail', ['invoice' => $d->invoice_jual_id])}}">
+                        <a href="{{route('billing.invoice-jual.detail', ['invoice' => $d->invoice_jual_id])}}">
                             {{$d->invoiceJual->kode}}
                         </a>
                         @endif
